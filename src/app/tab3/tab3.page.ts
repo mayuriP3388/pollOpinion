d4b1925c36dcdf10eb1490b4c347d5ab76d4e811
mayuriP3388 <84899599+mayuriP3388@users.noreply.github.com>
2022-11-18 15:01:10 +0530

    createpoll

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  question:any ='';
+  userOption:any
   constructor() {}
-
+  ngOnInit() {
+    
+this.userOption =[
+  {
+    id:1,
+    optionValue : '',
+    isVisible : true,
+    isDeleted:false,
+    placeholder: 'Option 1'
+  },
+  {
+    id:2,
+    optionValue : '',
+    isVisible : true,
+    isDeleted:false,
+    placeholder: 'Option 2'
+  },
+]
+}
+      addMore(){
+        if(this.userOption.length <= 3){
+          this.userOption.push({id :this.userOption.length +1,optionValue : '', isVisible : true ,isDeleted:true, placeholder: 'Option '+ (this.userOption.length+1)})
+          console.log(this.userOption)
+        }else{
+          console.log('Max 4')
+        }
+       
+      }
+      deleted(value:any,index:number){
+        this.userOption.splice(index,1)
+      }
+      post(){
+        var content =[]
+        for(let i =0; i<this.userOption.length;i++){
+          content.push(this.userOption[i].optionValue)
+        }
+        let postreq ={
+          'userId':1,
+          'question': this.question,
+          'options': content
+        }
+       console.log(postreq) 
+      }
 }
